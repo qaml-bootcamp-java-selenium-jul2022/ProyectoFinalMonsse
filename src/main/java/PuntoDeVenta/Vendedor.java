d18c6cbad22af1030328cc170ba6ef4eb4dcf480
Denisse Monsserrath Vargas Yañez <rouge0051@gmail.com>
2022-08-24 19:45:57 -0500

    Creacion del metodo de venta con su test correspondiete.

--- src/main/java/PuntoDeVenta/Vendedor.java
+++ src/main/java/PuntoDeVenta/Vendedor.java
@@ -1,4 +1,5 @@
 package PuntoDeVenta;
+import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 
@@ -10,11 +11,23 @@ public class Vendedor {
     }
     public void registrar(Cliente clienteNuevo){
         registgroDeClientes.put(clienteNuevo.getEmail(), clienteNuevo);
-
     }
-
     public boolean buscar (String email){
         return registgroDeClientes.containsKey(email);
+    }
+
+    public float venta(List<Prenda> prendasParaVender){
+        float totalVenta = 0;
+
+        System.out.println("PRENDA\t\t COSTO($)");
+        System.out.println("********************************************");
+        for (Prenda prenda: prendasParaVender) {
+            totalVenta += prenda.getCostoVenta();
+            System.out.println(prenda.getTipoDePrenda() + "\t\t\t" + "$ " + prenda.getCostoVenta());
+        }
+        System.out.println("********************************************");
+        System.out.println("TOTAL\t\t" + "$ " + totalVenta);
+        return totalVenta;
     }
 
 }
